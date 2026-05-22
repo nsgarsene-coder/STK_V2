0d52f7ccb7d0a16e68bcf1b0fade7a5c4302a09d
nsgarsene-coder <nsgarsene@gmail.com>
2026-05-22 09:50:12 +0200

    mise à jour 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import GameBoard from './components/GameBoard';
 import UserSpace from './components/UserSpace';
 import Tutorial from './components/Tutorial';
 import Modal from './components/Modal';
-import LoadingScreen from './components/Loadingscreen';
+import LoadingScreen from './components/abc';
 import { useGameState } from './state/gameState';
 import './App.css';
 
